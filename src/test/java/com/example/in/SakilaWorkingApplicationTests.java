982366109bcf687df7f7b778ca616905afa30486
pongredd <pongredd@LIN65004178.corp.capgemini.com>
2023-07-10 20:52:27 +0530

    After

--- src/test/java/com/example/in/SakilaWorkingApplicationTests.java
+++ src/test/java/com/example/in/SakilaWorkingApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.in;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SakilaWorkingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.in;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SakilaWorkingApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
